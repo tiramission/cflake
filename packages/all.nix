e343572cb4335045e06134fd4682c51ca9502395
tiramission <37465086+tiramission@users.noreply.github.com>
2025-07-20 01:10:33 +0800

    font only linux

--- packages/all.nix
+++ packages/all.nix
@@ -15,8 +15,9 @@ in
       if builtins.elem system supportedSystems
       then pkg
       else throw ("Current system " + system + " is not supported for this package. Supported: " + builtins.toString supportedSystems);
+    withLinuxs = withSystems ["x86_64-linux" "aarch64-linux"];
   in {
-    sarasa-term-sc-nerd = pkgs.callPackage ./sarasa-term-sc-nerd {};
+    sarasa-term-sc-nerd = withLinuxs (pkgs.callPackage ./sarasa-term-sc-nerd {});
     uv = pkgs.callPackage ./uv {};
     scrcpy = withSystems ["x86_64-linux" "aarch64-darwin"] (pkgs.callPackage ./scrcpy {});
     microsoft-edge = withSystems ["x86_64-linux"] (pkgs.callPackage ./microsoft-edge {});
