fcc8a686c9ceee9d22935e9414da8796f185fef5
tiramission <37465086+tiramission@users.noreply.github.com>
2025-09-16 20:05:57 +0800

    add wechat fix ime

--- packages/all.nix
+++ packages/all.nix
@@ -15,8 +15,10 @@ in
     sarasa-term-sc-nerd = pkgs.callPackage ./manual/sarasa-term-sc-nerd.nix {};
     smctemp = pkgs.callPackage ./manual/smctemp.nix {};
     microsoft-edge = pkgs.callPackage ./manual/microsoft-edge.nix {};
+        wechat = pkgs.callPackage ./manual/wechat/package.nix {};
 
     uv = pkgs.callPackage ./auto-update/uv {};
     scrcpy = pkgs.callPackage ./auto-update/scrcpy {};
     qq = pkgs.callPackage ./auto-update/qq {};
+
   })
