0f1d293fc699a07fc630f941cb0ede8e6e217adf
tiramission <37465086+tiramission@users.noreply.github.com>
2025-10-12 13:20:27 +0800

    fix lib error

--- lib/all.nix
+++ lib/all.nix
@@ -1,4 +1,6 @@
-inputs: {
+inputs: let
+  inherit (inputs.nixpkgs) lib;
+in {
   # 与 lib.optionals 相反的函数：先接受模块列表，再接受条件
   includeif = modules: condition: lib.optionals condition (lib.toList modules);
   # 选择同版本时优先级更高的包，即第一个包
