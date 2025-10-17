f9e0c38a40b926f80b33ba6c6e0573d276b57e92
tiramission <37465086+tiramission@users.noreply.github.com>
2025-10-17 21:12:01 +0800

    uv: remove proxy

--- packages/auto-update/uv/update.sh
+++ packages/auto-update/uv/update.sh
@@ -5,11 +5,11 @@ set -euo pipefail
 version=$(curl -fsSL https://api.github.com/repos/astral-sh/uv/releases/latest | nix run nixpkgs#yq -- .name -r)
 
 function url_for() {
-    echo "https://gh-proxy.com/github.com/astral-sh/uv/releases/download/${version}/uv-${1}.tar.gz"
+    echo "https://github.com/astral-sh/uv/releases/download/${version}/uv-${1}.tar.gz"
 }
 
 function hash_for() {
-    local url="https://gh-proxy.com/github.com/astral-sh/uv/releases/download/${version}/uv-${1}.tar.gz.sha256"
+    local url="https://github.com/astral-sh/uv/releases/download/${version}/uv-${1}.tar.gz.sha256"
     local sha256=$(curl -sSL "${url}" | awk '{print $1}')
     nix hash convert --hash-algo sha256 --to sri ${sha256}
 }
